b65167d97258c6784de5d3bc87edc12c40721b68
rizamasta <rizamasta@gmail.com>
2015-06-15 13:59:52 +0700

    beru

--- master/js/custom/config_base.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by rizamasta on 5/10/15.
- */
-function base_url(url) {
-  return "http://localhost/memo/"+url;
-}
-function base_gateway(url){
-  return "http://localhost:1200/"+url;
-}
